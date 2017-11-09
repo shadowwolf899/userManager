00ef915921579245dea64905aabca92e1ab28214
Jericho Keyne <jerichokeyne@gmail.com>
2017-11-09 16:29:39 +0000

    Moved listusers into main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+function listUsers() {
+	_l="/etc/login.defs"
+	_p="/etc/passwd"
+
+	## get mini UID limit ##
+	l=$(grep "^UID_MIN" $_l)
+
+	## get max UID limit ##
+	l1=$(grep "^UID_MAX" $_l)
+
+	## use awk to print if UID >= $MIN and UID <= $MAX and shell is not /sbin/nologin   ##
+	awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $0 }' "$_p" | cut -f1 -d":" | tee users.txt
+}
+
 if [ "$EUID" -ne 0 ]
 	then echo "Please run as root"
 	exit 1
@@ -19,7 +34,8 @@ echo "Configure user accounts?(y/N) "
 read reply
 if [[ $reply == "y" ]]; then
 	echo "USERS:"
-	./listusers
+	#./listusers
+	listUsers
 	echo
 	echo "Type admins:"
 	while [ -z "$finAD" ] 
